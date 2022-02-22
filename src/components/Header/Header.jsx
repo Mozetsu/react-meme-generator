4b8d460f1163b31f175c79a6405bc3f8566392e0
Mozetsu <hello@mozetsu.dev>
2022-02-22 20:50:17 +0000

    Start building Form component.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import './header.css';
-import troll from '../../svg/troll.svg';
 
 export default function Header() {
 	return (
 		<section className="header">
-			<img className="header__logo" src={troll} alt="" />
-			<p>React course - Project 3</p>
+			<h3 className="header__title">Meme Generator</h3>
+			<p className="header__description">React course - Project 3</p>
 		</section>
 	);
 }
