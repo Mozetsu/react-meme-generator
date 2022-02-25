5dfb877abf68bab4521f4541dd0447b864975b69
Mozetsu <hello@mozetsu.dev>
2022-02-25 18:51:27 +0000

    Move state to App component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,55 @@ import Header from './components/Header/Header';
 import Form from './components/Form/Form';
 import Image from './components/Image/Image';
 import './app.css';
+import memesData from './utils/data';
 
 export default function App() {
-	const [url, setUrl] = useState('');
+	const [memeData, setMemeData] = useState({
+		topText: '',
+		bottomText: '',
+		url: '',
+	});
 
-	const setURL = (url) => setUrl(url);
+	const getRandomUrl = () => {
+		const { memes } = memesData.data;
+		const randomIndex = Math.floor(Math.random() * memes.length);
+		const { url } = memes[randomIndex];
+		return url;
+	};
+
+	const setImageUrl = (url) => {
+		setMemeData((prevMemeData) => ({
+			...prevMemeData,
+			url,
+		}));
+	};
+
+	const handleData = (event) => {
+		const { value, name } = event.target;
+
+		setMemeData((prevMemeData) => ({
+			...prevMemeData,
+			[name]: value,
+		}));
+	};
+
+	const handleNewMeme = (e) => {
+		e.preventDefault();
+		setImageUrl(getRandomUrl());
+	};
+
+	const formProps = {
+		setImageUrl,
+		handleData,
+		getRandomUrl,
+		handleNewMeme,
+	};
 
 	return (
 		<section className="app">
 			<Header />
-			<Form setURL={setURL} />
-			<Image url={url} />
+			<Form {...formProps} />
+			<Image url={memeData.url} />
 		</section>
 	);
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,27 +1,17 @@
 import React, { useEffect } from 'react';
 import './form.css';
-import memesData from '../../utils/data';
 
 const Form = (props) => {
-	useEffect(() => props.setURL(getRandomUrl()), []);
-
-	const getRandomUrl = () => {
-		const { memes } = memesData.data;
-		const randomIndex = Math.floor(Math.random() * memes.length);
-		const { url } = memes[randomIndex];
-		return url;
-	};
-
-	const handleNewMeme = (e) => {
-		e.preventDefault();
-		props.setURL(getRandomUrl());
-	};
+	useEffect(() => {
+		const randomUrl = props.getRandomUrl();
+		props.setImageUrl(randomUrl);
+	}, []);
 
 	return (
 		<form className="form">
 			<div className="form__input">
-				<input className="input" type="text" placeholder="Text top" />
-				<input className="input" type="text" placeholder="Text bottom" />
+				<input className="input" type="text" placeholder="Text top" name="topText" onChange={props.handleData} />
+				<input className="input" type="text" placeholder="Text bottom" name="bottomText" onChange={props.handleData} />
 			</div>
 			<input className="form__submit" type="submit" value="Generate meme" onClick={(e) => handleNewMeme(e)} />
 		</form>
